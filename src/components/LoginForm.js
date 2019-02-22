bdb8571425f4ed5c92bec49d8f4966cd87d09d79
reduviidae <wiljago@gmail.com>
2019-02-22 13:42:51 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,39 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
 
   render() {
+    const inputHandler = (e) => {
+     this.setState({
+       [e.target.id]: e.target.value
+     })
+    }
+
+    const validate = (e) => {
+      e.preventDefault();
+      if(this.state.username.length > 0 && this.state.password.length > 0) {
+        this.props.onSubmit(this.state)
+      }
+
+    }
+
     return (
-      <form>
+      <form onSubmit={(e)=>validate(e, this.state)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={inputHandler} id="username" name="username" type="text" value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={inputHandler} id="password" name="password" type="password" value={this.state.password}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,31 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: "",
+      counter: 0
+    };
   }
+
+  changer(e) {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  counter() {
+    this.setState({
+      counter: this.state.value.length
+    })
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={e => {this.changer(e); this.counter()}}/>
+        <h3>Remaining characters:{this.props.maxChars - this.state.counter}</h3>
       </div>
     );
   }
