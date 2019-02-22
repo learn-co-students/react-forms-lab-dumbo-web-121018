2e6c228f2884fc2f46e3122c5b9bbe561903abcc
github username <github email address>
2019-02-22 10:40:51 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,43 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+  change = event => {
+    console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  submitted = event => {
+    event.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    if(username && password) {
+      this.props.onSubmit({username, password})
+    }
+  }
+
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.submitted(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={event => this.change(event)} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={event => this.change(event)} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,22 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    this.state = {
+      // get the current length of the input
+      length: 0
+    };
+  }
 
-    this.state = {};
+  messageLength = event => {
+    this.setState({length: event.target.value.length})
+    console.log((this.props.maxChars) - (this.state.length))
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" onChange={this.messageLength} />
       </div>
     );
   }
